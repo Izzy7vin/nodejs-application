4b48c74ca9e935553083945ef7ab5ec0f70472d2
Israel Okonkwo <vinceisrael7@gmail.com>
2026-03-23 20:26:11 -0400

    Change SonarQube server URL in sonar-project.js
    Updated SonarQube server URL for project configuration.

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.236.152.81:9000/',
+  serverUrl: 'http://34.229.19.172:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
